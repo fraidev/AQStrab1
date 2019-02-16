3fb22dcb068ccd1af501b031f23333c9a35ac6e7
Felipe Cardozo <felipe.cardozo@sapiensia.com>
2019-02-16 03:00:33 -0200

    remove dao

--- src/main/java/br/com/fsma/projeto_web/bean/LoginBean.java
+++ src/main/java/br/com/fsma/projeto_web/bean/LoginBean.java
@@ -15,10 +15,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
 
-import br.com.fsma.projeto_web.modelo.dao.UsuarioDao;
-import br.com.fsma.projeto_web.modelo.negocio.Usuario;
-import br.com.fsma.projeto_web.tx.Transacional;
-
 @Named
 @ViewScoped
 public class LoginBean implements Serializable {
@@ -29,43 +25,9 @@ public class LoginBean implements Serializable {
 
 	@Inject
 	private HttpSession session;
-	@Inject
-	private UsuarioDao usuarioDao;
-	private Usuario usuario;
-	
-	public Usuario getUsuario() {
-		return usuario;
-	}
 
 	@PostConstruct
 	public void init() {
 		System.out.println("LoginBean.init();");
-		usuario = (Usuario) session.getAttribute(USUARIO_LOGADO);
-		if (usuario == null) {
-			usuario = new Usuario();
-		}
 	}
-	
-	@Transacional
-	public String loga() {
-		Usuario usuarioAutenticado  = usuarioDao.buscaUsuarioPelaAutenticacao(this.usuario);
-		if (usuarioAutenticado != null) {
-			usuarioAutenticado.setDataDoUltimoAcesso(LocalDateTime.now());
-			usuarioDao.atualiza(usuarioAutenticado);
-			session.setAttribute(USUARIO_LOGADO, usuarioAutenticado);
-			usuario = usuarioAutenticado;
-			return "/view/menu/menu.xhtml?faces-redirect=true";
-		}
-		String mensagem = "Usuário e/ou senha não inválido!";
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error!", mensagem));
-		return null;
-	}
-
-	public String desloga() {
-		session.removeAttribute(USUARIO_LOGADO);
-		session.invalidate();
-		usuario = new Usuario();
-		return "/view/login/login.xhtml?faces-redirect=true";
-	}
-	
 }
